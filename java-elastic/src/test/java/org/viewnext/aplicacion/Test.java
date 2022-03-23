4b29a7092384973fcf759f4e68928d3c4375b8c4
6002041 <6002041@PF2SJKYM.mshome.net>
2022-03-23 12:32:44 +0100

    TestProducto

---
+++ java-elastic/src/test/java/org/viewnext/aplicacion/Test.java
@@ -0,0 +1,21 @@
+package org.viewnext.aplicacion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void TestCreacionProducto() {
+		Producto producto1 = new Producto("id", "nombre", 10.00);
+		Producto producto2 = new Producto();
+		producto2.setId("id");
+		producto2.setName("nombre");
+		producto2.setPrice(20.00);
+		
+		assertTrue(producto1.getName().equals(producto2.getName()));
+		assertTrue(producto1.getId().equals(producto2.getId()));
+		assertTrue(producto1.getPrice()!=producto2.getPrice());
+		assertFalse(producto1.equals(producto2));
+	}
+
+}
